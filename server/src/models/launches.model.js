cc5e130e56ebd1af27c216916ca15ae742af8d94
Vasil Evgeniev Aleksandrov <vvvaleksandrovv@gmail.com>
2023-12-11 10:37:59 +0200

    added launches database methods to controller

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,6 @@
+const launchesDataBase = require('./launches.mongo');
+const planets = require('./planets.mongo');
+
 const launches = new Map();
 
 let latestFlightNumber = 100;
@@ -13,10 +16,29 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+saveLaunch(launch)
+
+
+async function getAllLaunches() {
+  return await launchesDataBase.find({}, {'_id': 0, '__v': 0});
+}
+
+async function saveLaunch(launch) {
+
+  const planet = await planets.findOne({
+    keplerName: launch.target
+  });
+
+  if(!planet) {
+    throw new Error('No matching planet found')
+  }
+
+  await launchesDataBase.updateOne({
+    flightNumber: launch.flightNumber,
+  }, launch, {
+    upsert: true
+  })
 }
 
 function addNewLaunch(launch) {
